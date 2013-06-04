459d0317fce26e8bba56b4ec07ca4855874715ec
Matthew Smith <matthew@digivation.net>
2013-06-04 10:56:55 -0700

    first attempt at header.php

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
     <head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta description="<?php bloginfo( 'description' ); ?>" />
         <title><?php wp_title( '|', true, 'right' ); ?></title>
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
@@ -28,6 +29,7 @@
         <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
         <![endif]-->
         
+        <?php // TODO ::: MODIFY JS LOADS ?>
         <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
         <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.9.1.js"></script>
         <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery/jquery-scrolltofixed-min.js"></script>
@@ -47,7 +49,8 @@
             <div class="wrapper">
                 <header class="header">
                     <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><img src="include/images/logo.jpg" rel="home" alt="img"></a>
-                    <nav class="navigation">
+                    <nav class="navigation" role="navigation">
+                        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'container' => false ) ); ?>
                         <ul>
                             <li><a href="#">Blog</a></li>
                             <li><a class="active-nav" href="#">Tech</a></li>
@@ -56,7 +59,8 @@
                         </ul>
                     </nav>
                     <!--mobile navigation-->
-                    <nav class="nav">
+                    <nav class="nav" role="navigation">
+                    	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'container' => false ) ); ?>
                         <ul>
                             <li class="current"><a href="#">Blog</a></li>
                             <li><a href="#">Tech</a></li>
@@ -70,24 +74,4 @@
                         <input class="textbx" type="text" value="" onfocus="if(this.value=='')this.value=''" onblur="if(this.value=='')this.value=''"/>
                     </div>
                 </header>
-
-
-
-
-			<h1 class="site-title"><a href="" title="" ><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-
-		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
-		<?php endif; ?>
-	</header><!-- #masthead -->
-
-	<div id="main" class="wrapper">
+                <!-- REMOVE : end of header.php -->
