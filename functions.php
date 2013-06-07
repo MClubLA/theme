b58073cb34336a1e60975c91b6b81a32f6981634
Matthew Smith <matthew@digivation.net>
2013-06-06 20:55:13 -0700

    fixing nav

--- functions.php
+++ functions.php
@@ -175,14 +175,14 @@ add_filter( 'wp_title', 'twentytwelve_wp_title', 10, 2 );
  * Makes our wp_nav_menu() fallback -- wp_page_menu() -- show a home link.
  *
  * @since Twenty Twelve 1.0
- *
+ */
 function twentytwelve_page_menu_args( $args ) {
 	if ( ! isset( $args['show_home'] ) )
 		$args['show_home'] = true;
 	return $args;
 }
 add_filter( 'wp_page_menu_args', 'twentytwelve_page_menu_args' );
-*/
+
 /**
  * Registers our main widget area and the front page widget areas.
  *
