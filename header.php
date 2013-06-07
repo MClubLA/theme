c9123ae2ee6f0aea89ba82e85c752a7c5b7487fc
Matthew Smith <matthew@digivation.net>
2013-06-06 19:19:47 -0700

    updated header

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
         <div class="outer-wrapper">
             <div class="wrapper">
                 <header class="header">
-                    <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><img src="include/images/logo.jpg" rel="home" alt="img"></a>
+                    <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" rel="home" alt="img"></a>
                     <nav class="navigation" role="navigation">
                         <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'container' => false ) ); ?>
                         <ul>
