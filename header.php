e74d01b9fbdcf5e98ef1d896bbb540360b1801ed
Matthew Smith <matthew@digivation.net>
2013-06-06 20:48:51 -0700

    fixing nav

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
             <div class="wrapper">
                 <header class="header">
                     <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.jpg" rel="home" alt="img"></a>
-                    
+                    <!--big nav-->
                     <?php wp_nav_menu( array( 'container' => 'nav',
 											  'container_class' => 'navigation',
 											  'theme_location' => 'primary', 
