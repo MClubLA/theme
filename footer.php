e9dd3b2c29dd5c85276529d8be91b6a8ccee3a03
Matthew Smith <matthew@digivation.net>
2013-06-20 11:33:46 -0700

    imported generated _s

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package MClub LA
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'mclub_credits' ); ?>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'mclub' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'mclub' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'mclub' ), 'MClub LA', '<a href="http://mclub.la" rel="designer">The Team</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
