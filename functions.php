16a13e9836434853f00f0f7b99166de6a7122fe7
Matthew Smith <matthew@digivation.net>
2013-06-20 13:03:07 -0700

    adding widget area for slider

--- functions.php
+++ functions.php
@@ -92,8 +92,16 @@ add_action( 'after_setup_theme', 'mclub_register_custom_background' );
  */
 function mclub_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'mclub' ),
+		'name'          => __( 'Right Sidebar', 'mclub' ),
 		'id'            => 'sidebar-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+	register_sidebar( array(
+		'name'          => __( 'Main Page Top Area', 'mclub' ),
+		'id'            => 'sidebar-2',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h1 class="widget-title">',
