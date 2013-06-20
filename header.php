e248121599e1843e2f612ac3bf58c2de91dcf55b
Matthew Smith <matthew@digivation.net>
2013-06-20 14:47:22 -0700

    moving scrolltofix code

--- header.php
+++ header.php
@@ -14,14 +14,12 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
 <?php wp_head(); ?>
-
-	<script type="text/javascript">
-		$(document).ready(function() {
-			$('header').scrollToFixed();
-		});
-	</script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('site-header').scrollToFixed();
+	});
+</script>
 </head>
 
 <body <?php body_class(); ?>>
