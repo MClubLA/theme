76d00d32287041cac503472d789a459a3ec3dc4f
Matthew Smith <matthew@digivation.net>
2013-06-20 14:42:16 -0700

    adding fixed header script

--- functions.php
+++ functions.php
@@ -114,11 +114,16 @@ add_action( 'widgets_init', 'mclub_widgets_init' );
  * Enqueue scripts and styles
  */
 function mclub_scripts() {
+
+	wp_register_script( 'jquery-scrolltofixed', get_template_directory_uri() . '/js/jquery-scrolltofixed-min.js', array('jquery') );
+
 	wp_enqueue_style( 'mclub-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'mclub-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'mclub-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	
+	wp_enqueue_script( 'mclub-fixednav', get_template_directory_uri() . '/js/mclub-fixednav.js', array('jquery-scrolltofixed') );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
