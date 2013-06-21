7ca2351695538f092dd0f7426f8dd6139bfc35ca
Matthew Smith <matthew@digivation.net>
2013-06-21 12:23:21 -0700

    remove pingback

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php wp_head(); ?>
 <script type="text/javascript">
 	$(document).ready(function() {
