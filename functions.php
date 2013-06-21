b4a18d209ee7e72d6cd5881d255cfd5de09b586b
Matthew Smith <matthew@digivation.net>
2013-06-20 20:37:06 -0700

    fixing sidebar

--- functions.php
+++ functions.php
@@ -40,6 +40,7 @@ function mclub_setup() {
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
 	add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 60, 60, true );
 
 	/**
 	 * This theme uses wp_nav_menu() in one location.
