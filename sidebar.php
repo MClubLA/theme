d1358001603c4ab9439398777ccc461553a0e7f3
Matthew Smith <matthew@digivation.net>
2013-07-19 17:36:32 -0700

    remove sidebar from single pages

--- sidebar.php
+++ sidebar.php
@@ -5,6 +5,8 @@
  * @package MClub LA
  */
 ?>
+<?php if ( !is_single() ) : ?>
+
 	<div id="secondary" class="widget-area" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
@@ -31,3 +33,4 @@
 
 		<?php endif; // end sidebar widget area ?>
 	</div><!-- #secondary -->
+<?php endif; ?>
