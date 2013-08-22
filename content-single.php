6d46be369c4d4c8fa03ddb6f8c472194147f329c
Matthew Smith <matthew@digivation.net>
2013-08-21 22:50:01 -0700

    [issue #3] comment out var_dump

--- content-single.php
+++ content-single.php
@@ -7,7 +7,7 @@
 	<?php /* Display leader media (uses ACF) */
 	if( get_field('mclub_leader_image') ) : 
 		$mclub_leader_image = get_field('mclub_leader_image');
-		var_dump( $mclub_leader_image );
+		//var_dump( $mclub_leader_image );
 	?>
     <div class="leader-media">
     	<img src="<?php echo $mclub_leader_image["url"]; ?>" alt="<?php echo $mclub_leader_image["alt"]; ?>" />
