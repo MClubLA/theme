4f3d7752545e76fe7ad8eb40fdce6587327bc357
Matthew Smith <matthew@digivation.net>
2013-08-22 00:06:59 -0700

    [issue #3] adding debug code

--- single.php
+++ single.php
@@ -17,18 +17,14 @@ get_header(); ?>
 		<?php /* Display leader media (uses ACF) */
         if( get_field('mclub_leader_image') || get_field('mclub_leader_external') ) : 
 			// image and/or external link attached to the post - which is selected?
-			var_dump( get_field('mclub_leader_image') );
-			var_dump( get_field('mclub_leader_external') );
-			var_dump( get_field('mclub_leader_image_or_video') );
-
 			switch( get_field('mclub_leader_image_or_video') ) {
-				case "image":
+				case "Image":
 					echo "IMAGE SELECTED";
 					$mclub_leader_image = get_field('mclub_leader_image');
 					//var_dump( $mclub_leader_image );
 					break;
-				case "external":
-					echo "EXTERNAL SELECTED";
+				case "Video":
+					echo "VIDEO SELECTED";
 					$mclub_leader_code = get_field('mclub_leader_external');
 					break;
 			}
