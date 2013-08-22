21228eb6d02ee733b868147d52f92f5313a47907
Matthew Smith <matthew@digivation.net>
2013-08-21 22:47:07 -0700

    modifiy content_width variable to more accurately reflect content

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 640; /* pixels */
+	$content_width = 860; /* pixels */
 
 if ( ! function_exists( 'mclub_setup' ) ) :
 /**
