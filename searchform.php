946198d96616f4ae94d0000b515b79ffcb076114
Matthew Smith <matthew@digivation.net>
2013-08-22 10:06:57 -0700

    [issue #10] fixed

--- searchform.php
+++ searchform.php
@@ -8,5 +8,5 @@
 	<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 		<label for="s" class="screen-reader-text"><?php _ex( 'Search', 'assistive text', 'mclub' ); ?></label>
 		<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" />
-		<input type="submit" class="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'mclub' ); ?>" />
+		<input type="submit" class="submit" id="searchsubmit" value="<?php //echo esc_attr_x( 'Search', 'submit button', 'mclub' ); ?>" />
 	</form>
