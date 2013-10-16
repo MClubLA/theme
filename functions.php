ff610982f33ac635e9f685b9c72ae71a6d87413a
Matthew Smith <matthew@digivation.net>
2013-10-16 14:53:33 -0400

    [iss11] add link to responsive stylesheet

--- functions.php
+++ functions.php
@@ -98,6 +98,8 @@ function mclub_scripts() {
 
 	wp_enqueue_style( 'mclub-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'mclub-responsive', get_template_directory_uri() . 'responsive.css' );
+
 	wp_enqueue_script( 'mclub-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'mclub-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
