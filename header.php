8e72ff51bbcbc7d7cfdd2f45b166cc1cf9482f5b
Matthew Smith <matthew@digivation.net>
2013-10-22 00:01:26 -0400

    added viewport and initial scale meta tags

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <?php wp_head(); ?>
