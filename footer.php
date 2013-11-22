c0f0c6ef2cffe3723b6db104975e5115c07b8ce4
Matthew Smith <matthew@digivation.net>
2013-11-22 00:41:46 -0500

    modify footer to use widget areas

--- footer.php
+++ footer.php
@@ -14,24 +14,32 @@
 		<div class="footer-wrapper">
 			<div id="footer-banner"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-banner.png" alt="MClub LA - Motorsports is Life" /></div>
 			<div id="footer-col-1" class="footer-column">
+				<?php if ( ! dynamic_sidebar( 'footer-col-1' ) ) : ?>
 				<ul>
 					<li class="footer-col-header">Help</li>
-					<li><a href="#">xxx</a></li>
-					<li><a href="#">xxx</a></li>
-					<li><a href="#">xxx</a></li>
+					<li><a href="#">Shop Policies</a></li>
+					<li><a href="#">Contact</a></li>
+					<li><a href="#">My Account</a></li>
 				</ul>
+				<?php endif; ?>
 			</div>
 			<div id="footer-col-2" class="footer-column">
+				<?php if ( ! dynamic_sidebar( 'footer-col-2' ) ) : ?>
 				<ul>
 					<li class="footer-col-header">Company</li>
-					<li><a href="#">xxx</a></li>
+					<li><a href="#">About</a></li>
 				</ul>
+				<?php endif; ?>
 			</div>
 			<div id="footer-col-3" class="footer-column">
+				<?php if ( ! dynamic_sidebar( 'footer-col-3' ) ) : ?>
 				<ul>
 					<li class="footer-col-header">Site Tools</li>
-					<li><a href="#">xxx</a></li>
-					<li><a href="#">xxx</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Tech</a></li>
+					<li><a href="#">Forum</a></li>
+				</ul>
+				<?php endif; ?>
 			</div>
 			<div id="footer-col-email"></div>
 			<div id="footer-col-social-buttons"></div>
