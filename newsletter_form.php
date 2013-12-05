c8d71f9e7d60a13c38bc14f97190d18bad8eedb4
Matthew Smith <matthew@digivation.net>
2013-12-04 23:10:25 -0500

    change placeholder text

--- newsletter_form.php
+++ newsletter_form.php
@@ -10,7 +10,7 @@
 <!-- Begin MailChimp Signup Form -->
 <div id="mc_embed_signup">
     <form action="http://mclub.us4.list-manage.com/subscribe/post?u=23657158bd06328e132cdada8&amp;id=846779040b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-        <input type="email" value="" placeholder="email address" name="EMAIL" class="required email" id="mce-EMAIL">
+        <input type="email" value="" placeholder="email for newsletter" name="EMAIL" class="required email" id="mce-EMAIL">
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;">
             <input type="text" name="b_23657158bd06328e132cdada8_846779040b" value="">
