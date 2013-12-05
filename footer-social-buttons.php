e12a78688e0220d497fca15a8be3b068c152c510
Matthew Smith <matthew@digivation.net>
2013-12-04 23:39:26 -0500

    add social button links

--- footer-social-buttons.php
+++ footer-social-buttons.php
@@ -7,3 +7,6 @@
  * @package MClub LA
  */
 ?>
+<a class="footer-social social-button-facebook" href="https://www.facebook.com/MClubLA" target="_blank"></a>
+<a class="footer-social social-button-twitter" href="https://twitter.com/MClubLA" target="_blank"></a>
+<a class="footer-social social-button-instagram" href="https://instagram.com/MClubLA/" target="_blank"></a>
