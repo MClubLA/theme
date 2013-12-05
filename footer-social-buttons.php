c7684039c637d431c44976ac7336503e34a05c71
Matthew Smith <matthew@digivation.net>
2013-12-05 00:35:28 -0500

    move images into html

--- footer-social-buttons.php
+++ footer-social-buttons.php
@@ -7,6 +7,6 @@
  * @package MClub LA
  */
 ?>
-<a id="social-button-facebook" class="footer-social" href="https://www.facebook.com/MClubLA" target="_blank"></a>
-<a id="social-button-twitter" class="footer-social" href="https://twitter.com/MClubLA" target="_blank"></a>
-<a id="social-button-instagram" class="footer-social" href="https://instagram.com/MClubLA/" target="_blank"></a>
+<a id="social-button-facebook" class="footer-social" href="https://www.facebook.com/MClubLA" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-button-facebook.png" alt="MClub LA is on Facebook" /></a>
+<a id="social-button-twitter" class="footer-social" href="https://twitter.com/MClubLA" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-button-twitter.png" alt="MClub LA is on Twitter" /></a>
+<a id="social-button-instagram" class="footer-social" href="https://instagram.com/MClubLA/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-button-instagram.png" alt="MClub LA is on Instagram" /></a>
