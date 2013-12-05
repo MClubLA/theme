4b8b2e15d0b50369d21f6230947f260736e35f63
Matthew Smith <matthew@digivation.net>
2013-12-05 00:49:09 -0500

    testing

--- footer-social-buttons.php
+++ footer-social-buttons.php
@@ -7,6 +7,6 @@
  * @package MClub LA
  */
 ?>
-<a id="social-button-facebook" class="footer-social" href="https://www.facebook.com/MClubLA" target="_blank"><i class="fa fa-facebook-square"></i></a>
+<a id="social-button-facebook" class="footer-social" href="https://www.facebook.com/MClubLA" target="_blank"><i class="fa fa-facebook-square"></i>test</a>
 <a id="social-button-twitter" class="footer-social" href="https://twitter.com/MClubLA" target="_blank"><i class="fa fa-twitter"></i></a>
 <a id="social-button-instagram" class="footer-social" href="https://instagram.com/MClubLA/" target="_blank"><i class="fa fa-instagram"></i></a>
