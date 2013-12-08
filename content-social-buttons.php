87709374ee536c152d78cf150447dda4e884bf3b
Matthew Smith <matthew@digivation.net>
2013-12-08 14:56:48 -0500

    add twitter

--- content-social-buttons.php
+++ content-social-buttons.php
@@ -9,3 +9,4 @@
 ?>
 
 <div class="fb-share-button" data-href="<?php the_permalink(); ?>" data-type="icon"></div>
+<div class="tw-share-button"><a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" data-count="none" data-hashtags="MClubLA"></a>
