7cfbde05d229626dd5e26e401ed2001396e91593
Matthew Smith <matthew@digivation.net>
2013-12-08 15:07:31 -0500

    move jssdk to a function

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 </script>
 <?php // Only include FB JavaScript SDK on single pages (where the social buttons are used)
 	if( is_singular() ) {
-		echo '<script>(function(e,t,n){var r,i=e.getElementsByTagName(t)[0];if(e.getElementById(n))return;r=e.createElement(t);r.id=n;r.src="//connect.facebook.net/en_US/all.js#xfbml=1&appId=563087663768939";i.parentNode.insertBefore(r,i)})(document,"script","facebook-jssdk")</script>';
+		
 		echo "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
 	}
 ?>
