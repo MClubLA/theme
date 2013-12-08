eda4695d21a603e0f08df5186d5c1770c1e4b6d5
Matthew Smith <matthew@digivation.net>
2013-12-08 17:37:18 -0500

    test

--- functions.php
+++ functions.php
@@ -241,10 +241,11 @@ add_filter( 'excerpt_more', 'mclub_excerpt_more' );
 /**
  * Breadcrumb Type Filter to fix classes
  */
-function mclub_bnc_types( $content, $id ) {
+function mclub_bnc_types( $content ) {
 	return array_diff($content, array('post'));
 }
-//add_filter( 'bcn_breadcrumb_types','mclub_bcn_types',10,2 );
+add_filter( 'bcn_breadcrumb_types','mclub_bcn_types' );
+
 /**
  * Improves the caption shortcode with HTML5 figure & figcaption; microdata
  * 
